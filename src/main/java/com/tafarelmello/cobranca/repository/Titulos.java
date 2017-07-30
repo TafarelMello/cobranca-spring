c097cc06916977f8cc09db630afe6854d8f668de
Tafarel Mello <tafarel.mello@gmail.com>
2017-07-29 22:56:02 -0300

    abc

--- src/main/java/com/tafarelmello/cobranca/repository/Titulos.java
+++ src/main/java/com/tafarelmello/cobranca/repository/Titulos.java
@@ -3,9 +3,11 @@ package com.tafarelmello.cobranca.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tafarelmello.cobranca.model.Titulo;
 
+@Repository
 public interface Titulos extends JpaRepository<Titulo, Long> {
 
 	public List<Titulo> findByDescricaoContaining(String descricao);
